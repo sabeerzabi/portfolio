d6b28611d5f7d4372b5c1eb4dd7de114ec7d357f
root <root@localhost.localdomain>
2019-06-08 20:24:09 +0530

    counter application

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,40 +1,15 @@
 import React, { Component } from 'react'
 
 class Counter extends Component {
-    state = {
-        value:  this.props.counter.value,
-        key:this.props.counter.id
-    };
-
-    constructor(props) {
-        super(props);
-        this.test = this.test.bind(this);
-    }
-    test(){
-
-    }
-    incrementCount = product =>{
-        this.setState({
-           value: this.state.value + 1
-        });
-    };
-    decrementCount = product =>{
-        if(this.state.value === 0){
-            return null;
-        }
-        this.setState({
-           value: this.state.value - 1
-        });
-    };
     render() { 
-        let key = this.props.counter.id;
+        let counter = this.props.counter;
         return ( 
             <React.Fragment>
                 {this.props.children}
                 <div className="text-danger">
                     <span className="badge badge-primary p-2 m-2">{this.formatCount()}</span>
-                    <button className="btn btn-success btn-sm" onClick={() => this.incrementCount(key)}>Add</button>
-                    <button className="btn btn-warning ml-2 btn-sm" onClick={() => this.decrementCount(key)}> Remove</button>
+                    <button className="btn btn-success btn-sm" onClick={() => this.props.incrementCount(counter)}>Add</button>
+                    <button className="btn btn-warning ml-2 btn-sm" onClick={() => this.props.decrementCount(counter)}> Remove</button>
                     <button className="btn btn-danger ml-2 btn-sm" onClick={() => this.props.onDelete(this.props.counter.id)}> Delete</button>
                 </div> 
             </React.Fragment>
@@ -42,7 +17,7 @@ class Counter extends Component {
     }
 
     formatCount = ()=>{
-        const { value } = this.state;
+        const { value } = this.props.counter;
         return value;
     }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,6 +12,42 @@ class Counters extends Component {
         ],
         imageUrl: "http://picsum.photos/id/500/500/50?grayscale"
     };
+    /* Inrement */
+    incrementCount = counter =>{
+        /* Clone data */
+        let counters = [...this.state.counters];
+        let index = counters.indexOf(counter);
+        counters[index].value = counter.value + 1;
+        this.setState({counters});
+    };
+    /* Count */
+    totalCount = () =>{
+        return this.state.counters.reduce((total, counter) =>{
+            return total + counter.value;
+        }, 0)
+    }
+    /* Decrement  */
+    decrementCount = counter =>{
+        let counters = [...this.state.counters];
+        let index = counters.indexOf(counter);
+        if(counter.value  === 0){
+            return null;
+        }
+        counters[index].value = counter.value - 1;
+        this.setState({counters});
+    };
+    /* Reset  */
+    resetCount = () =>{
+        /* Clone data */
+        let counters = [...this.state.counters];
+        counters = counters.map(c =>{
+            let counter = {...c};
+            counter.value = 0;
+            return counter;
+        });
+        this.setState({counters});
+    };
+    /* Delete */
     handleDelete = (counterId) =>{
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({counters});
@@ -20,9 +56,10 @@ class Counters extends Component {
         return (            
             <React.Fragment>
                 <div style= {{ backgroundImage: this.state.imageUrl}}>
+                    <button className="btn btn-info btn-lg" onClick={this.resetCount}>Reset ({this.totalCount()})</button>
                     {this.state.counters.map(counter => 
-                        <Counter key={counter.id} counter={counter} onDelete={this.handleDelete}>
-                            <p>---------------------</p>
+                        <Counter key={counter.id} counter={counter} onDelete={this.handleDelete} incrementCount={this.incrementCount} decrementCount={this.decrementCount}>
+                            <div className="m-0 p-0 text-left text-danger">Product - {counter.id}</div>
                         </Counter>
                     )}
                 </div>
